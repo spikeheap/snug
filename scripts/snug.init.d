84b4bc2fae516bd93c94a31cbe9bdff4f607308f
root <root@oemgateway.(none)>
2013-11-27 20:00:53 +0000

    adding init script

---
+++ scripts/snug.init.d
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Init script courtesy of http://blog.rueedlinger.ch/2013/03/raspberry-pi-and-nodejs-basic-setup/
+#
+# Add this file to your startup directory using the following command:
+#
+#   $ ln -s /usr/local/snug/scripts/snug.init.d /etc/init.d/snug
+#
+
+NODE=/opt/node/bin/node
+SERVER_JS_FILE=/usr/local/snug/app/server.js
+USER=snug
+OUT=/dev/null #If you're not using IPE and want to log, just set your log file path here.
+
+case "$1" in
+
+start)
+	echo "starting node: $NODE $SERVER_JS_FILE"
+	sudo -u $USER $NODE $SERVER_JS_FILE > $OUT 2>$OUT &
+	;;
+
+stop)
+	killall $NODE
+	;;
+
+*)
+	echo "usage: $0 (start|stop)"
+esac
+
+exit 0
